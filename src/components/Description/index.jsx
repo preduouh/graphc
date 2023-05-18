73d6c2fccfd16cef433be819b951cb08b7cd0eb0
preduouh <58116134+preduouh@users.noreply.github.com>
2023-05-18 15:28:25 -0300

    Add files via upload

--- src/components/Description/index.jsx
+++ src/components/Description/index.jsx
@@ -8,6 +8,7 @@ function Description(){
             <SubTitle> É uma pá...</SubTitle>
             <SubTitle> Uma bela ferramenta</SubTitle>
             <SubTitle>Faz Buracos </SubTitle>
+            <SubTitle>Por R$:20</SubTitle>
             <Title> \ (•◡•) / </Title>
         </Container>
     );
