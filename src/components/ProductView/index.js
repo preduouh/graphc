342802e4f7e301c4f511a1da04573773abde8f44
preduouh <58116134+preduouh@users.noreply.github.com>
2023-05-18 20:54:39 -0300

    Update index.js

--- src/components/ProductView/index.js
+++ src/components/ProductView/index.js
@@ -1,5 +1,5 @@
 import { Canvas, useFrame } from '@react-three/fiber';
-import { OrbitControls, Stars , useGLTF } from '@react-three/drei';
+import { OrbitControls, useGLTF } from '@react-three/drei';
 import { useRef } from 'react';
 
 function Shovel(){
@@ -9,19 +9,16 @@ function Shovel(){
     shoRef.current.rotation.y += 0.01;
   });
   
-  return <primitive ref ={shoRef}object={scene} scale={1} />;
+  return <primitive ref ={shoRef} object={scene} scale={1} />;
 }
-
 function ProductView() {
 
   return <Canvas camera={{ fov: 9, position: [-10, 45, 20]}}
                   style={{height: window.innerHeight - 90}}> 
   <OrbitControls />
-  <Stars/>
   <ambientLight intensity={0.5}/> 
   <spotLight position={[10, 15, 10]} angle={0.3}/>
   <Shovel/>
   </Canvas>;
 }
-  
   export default ProductView;
